1dd7d1738a1d94dcaa31e78c254d755e39a46529
Andy <none>
2022-07-07 16:21:50 +0200

    grid component added

---
+++ src/components/grid/grid.component.jsx
@@ -0,0 +1,16 @@
+import Row from '../row/row.component';
+
+const Grid = ({ currentGuess, guesses, turn }) => {
+	return (
+		<div>
+			{guesses.map((guess, index) => {
+				if (turn === index) {
+					return <Row key={index} currentGuess={currentGuess}></Row>;
+				}
+				return <Row key={index} guess={guess}></Row>;
+			})}
+		</div>
+	);
+};
+
+export default Grid;
